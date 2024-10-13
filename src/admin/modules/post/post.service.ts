bacda3d791bf119cf187a0cd6b15986d537e4957
Xepobopa <ismartsdn4477@gmail.com>
2024-10-13 02:22:04 +0200

    fix

--- src/admin/modules/post/post.service.ts
+++ src/admin/modules/post/post.service.ts
@@ -10,7 +10,7 @@ class PostService {
         // send to every user
         for (const user of users) {
             if (newPost.image !== undefined) {
-                bot.api.sendPhoto(user.chat_id, `${process.env.STATIC_FILES_URL}/${newPost.image}`)
+                bot.api.sendPhoto(user.chat_id, newPost.image)
                 .then(() => {
                     bot.api.sendMessage(user.chat_id, newPost.text, { parse_mode: 'HTML' });
                 });
